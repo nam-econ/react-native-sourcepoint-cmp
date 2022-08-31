a7bd427dda7e761b980f85866e52aaaa5c5eadda
Dominic Wroblewski <domness@gmail.com>
2022-08-31 21:24:22 +0100

    test

--- ios/SourcepointCmp.m
+++ ios/SourcepointCmp.m
@@ -6,6 +6,8 @@ RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject)
 
+RCT_EXTERN_METHOD(init)
+
 + (BOOL)requiresMainQueueSetup
 {
   return NO;
--- ios/SourcepointCmp.swift
+++ ios/SourcepointCmp.swift
@@ -1,5 +1,23 @@
+import Foundation
+import ConsentViewController
+
 @objc(SourcepointCmp)
 class SourcepointCmp: NSObject {
+
+    lazy var consentManager: SPConsentManager = { SPConsentManager(
+        accountId: 1,
+        propertyName: try! SPPropertyName("1"),
+        campaignsEnv: .Public,
+        campaigns: SPCampaigns(
+            gdpr: SPCampaign()
+        ),
+        delegate: self
+    )}()
+
+    @objc(init)
+    override init() {
+        super.init()
+    }
 
   @objc(multiply:withB:withResolver:withRejecter:)
   func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
